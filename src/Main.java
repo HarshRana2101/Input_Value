ea253e9a86a551a5f14f111566669571d083d82a
harsh <harshrana2101@gmail.com>
2020-07-14 13:26:09 -0400

    3.0

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter Your Name: ");
-        String name = scanner.nextLine();
-
         System.out.print("Enter your year of birth: ");
-        int yearofBirth = scanner.nextInt();
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
 
-        int age = 2020 - yearofBirth;
+            System.out.print("Enter Your Name: ");
+            String name = scanner.nextLine();
 
-        System.out.println("Your name is " + name +
-                " ,and you are " + age + " years old.");
+            int age = 2020 - yearOfBirth;
 
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name +
+                        " ,and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid Year of Bith");
+            }
+        } else {
+            System.out.println("unable to parse year of birth");
+        }
         scanner.close();
     }
 }
