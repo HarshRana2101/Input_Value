3e7514eded02cf1a4bbe3b7713c27cc138026b02
harsh <harshrana2101@gmail.com>
2020-07-14 13:09:44 -0400

    2.0

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter Your Name: ");
-        String name=scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Your name is "+name);
+        System.out.print("Enter Your Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your year of birth: ");
+        int yearofBirth = scanner.nextInt();
+
+        int age = 2020 - yearofBirth;
+
+        System.out.println("Your name is " + name +
+                " ,and you are " + age + " years old.");
+
         scanner.close();
     }
 }
